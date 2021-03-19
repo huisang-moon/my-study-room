a4adb271b2b8d7e78216c895bbb66f6fd3765c15
huisang-moon <78517217+huisang-moon@users.noreply.github.com>
2021-03-20 00:03:54 +0900

    Update lotto_cr.py

--- lotto_cr.py
+++ lotto_cr.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 custom_header = {
     'referer' : "http://www.naver.com",
-    'user_agent' : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
+    
     
 }
 
